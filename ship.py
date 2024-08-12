8c5cf48d86f6baae74f7769949e727e5f82943d4
Ray <771854195@qq.com>
2024-08-12 11:53:15 +0800

    first

---
+++ ship.py
@@ -0,0 +1,44 @@
+import pygame
+class Ship:
+    def __init__(self,ai_game):
+        #初始化飞船位置
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        #加载飞船图像，获取rect
+        self.image = pygame.image.load("images/ship5.bmp")
+        self.rect = self.image.get_rect()
+
+        #设置飞船位置
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        #self.y = float(self.rect.y)
+        
+        #新增移动标志位
+        self.moving_right = False
+        self.moving_left = False
+        #self.moving_up = False
+        #self.moving_down = False
+
+    
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            #self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        """if self.moving_up and self.rect.up > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.down < self.screen_rect.down:
+            self.y += self.settings.ship_speed"""
+        
+        self.rect.x = self.x
+        #self.rect.y = self.y
+
+    def blitme(self):
+        #绘制飞船
+        self.screen.blit(self.image,self.rect)
+
+
